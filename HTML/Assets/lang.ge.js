e5fcddeaa8fa05b28be0ebd06749f0555096790a
HelenaKilger <156218513+HelenaKilger@users.noreply.github.com>
2024-01-10 17:20:43 +0100

    Add files via upload

---
+++ HTML/Assets/lang.ge.js
@@ -0,0 +1,22 @@
+var CTATLanguagePack =
+{
+        "HORIZONTAL" : "LR", // Accepted values are: LR, RL
+        "VERTICAL" : "TB",      // Accepted values are TB, BT
+        "LOADING" : "Bitte warte, während der Tutor geladen wird",
+        "NEXTPROBLEM" : "Gleich geht es weiter...",
+        "CONGRATULATIONS_YOU_ARE_DONE": "Juhu! Weiter geht's.",
+        "SURE_YOU_ARE_DONE": "Bist du sicher, dass du fertig bist?",
+        "TUTORDISCONNECTED" : "Der Tutor hat die Verbindung unterbrochen. Bitte aktualisiere die Seite.",
+        "OUTOFORDER" : "Du musst zuerst andere Schritte ausführen, bevor du den Schritt ausführen, an dem du gerade gearbeitet hast. Du kannst einen Hinweis anfordern, um weitere Hilfe zu erhalten.",
+        "DONE" : "Fertig",
+        "HINT" : "Hinweis",
+        "NEXT" : "Weiter",
+        "PREVIOUS" : "Zurück",
+        "HIGHLIGHTEDSTEP" : "Schau dir an, was leuchtet.",
+        "NOTDONE" : "Es tut mir leid, aber du bist noch nicht fertig. Bitte arbeite weiter.",
+        "AUTHORPLEASECLOSE" : "Die Verbindung zu den Autorentools wurde unterbrochen. Bitte schließe diese Seite.",
+        "ERROR_502" : "Fehler bei der Kontaktaufnahme mit dem Server, bitte aktualisiere die Seite und versuche es erneut (HTTP-Status 502: Gateway Response).",
+        "ERROR_CONN_TS" : "Verbindung vom Nachhilfedienst abgelehnt, bitte überprüfe deinen Server oder wende dich an einen Administrator.",
+        "ERROR_CONN_LS" : "Der Logging-Server hat die Verbindung abgelehnt. Bitte überprüfe deine Verbindung oder wende dich an einen Administrator.",
+        "AWAITING_OTHER_COLLABORATORS": "Warten auf andere Teammitglieder..."
+};
